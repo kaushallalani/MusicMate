6550dfeb43a3e904dbe4057f17ebf5a85a593477
Dhanraj Mahida <119103043+dhanra1509@users.noreply.github.com>
2024-06-16 22:24:02 +0530

    integrated spotify sdk

--- lib/pages/search/index.dart
+++ lib/pages/search/index.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+import 'package:spotify_sdk/spotify_sdk.dart';
 
 class Search extends StatelessWidget {
   const Search({super.key});
+
+  Future<dynamic> spotifyLogin() async {
+      await SpotifySdk.connectToSpotifyRemote(clientId: '3222f0dac2e24c908781642f43c8506d', redirectUrl: 'musicmate://callback').then((res) {
+        Logger().d(res);
+      }).catchError((e){
+          Logger().d(e.toString());
+      });
+
+    return;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: const Color(0xffC4DFCB),
       child: Center(
-        child: Text(
-          "Page Search",
-          style: TextStyle(
-            color: Colors.green[900],
-            fontSize: 45,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
+        child:ElevatedButton( onPressed: spotifyLogin, child: const Text('Spotify'),)
       ),
     );
   }
